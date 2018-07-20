ea43e06823ab656a3bd5c8117f7f434d710ae04f
rossi21 <stephanie.rodriguez.garcia@gmail.com>
2018-07-20 11:16:18 -0500

    h

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="content-header">
-        <h1 class="pull-left">Lista de Clientes </h1>
+        <h1 class="pull-left">Lista </h1>
         <h1 class="pull-right">
            <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('clientes.create') !!}">Add New</a>
         </h1>
